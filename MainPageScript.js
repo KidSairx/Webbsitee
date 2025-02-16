033343c2c17657cbf027fdc040d27973d45b17a4
Sairx <ischevych@gmail.com>
2025-02-17 00:45:04 +0100

    commit

--- MainPageScript.js
+++ MainPageScript.js
@@ -1,7 +1,30 @@
-var CategoriesButton = document.getElementsByClassName("CategoriesButton");
+var AllCategoryButton = document.getElementById("AllCategoryButton");
+var GamingCategoryButton = document.getElementById("GamingCategoryButton");
+var SocialMediaCategoryButton = document.getElementById("SocialMediaCategoryButton");
+var ArtificialIntelligenceCategoryButton = document.getElementById("ArtificialIntelligenceCategoryButton");
+var WorldCategoryButton = document.getElementById("WorldCategoryButton");
+var AnimeAndMangaCategoryButton = document.getElementById("AnimeAndMangaCategoryButton");
 
-CategoriesButton.addEventListener("click", CategoriesButtonClicked);
+AllCategoryButton.addEventListener("click", function AllCategoryButtonClicked() {
+    console.log("Allcategory");
+});
 
-function CategoriesButtonClicked() {
-    console.log("CategoriesButtonClicked");
-};
+GamingCategoryButton.addEventListener("click", function GamingCategoryButtonClicked() {
+    console.log("Gamingcategory");
+});
+
+SocialMediaCategoryButton.addEventListener("click", function SocialMediaCategoryButtonClicked() {
+    console.log("SocialMediacategory");
+});
+
+ArtificialIntelligenceCategoryButton.addEventListener("click", function ArtificialIntelligenceCategoryButtonClicked() {
+    console.log("AIcategory");
+});
+
+WorldCategoryButton.addEventListener("click", function WorldCategoryButtonClicked() {
+    console.log("Worldcategory");
+});
+
+AnimeAndMangaCategoryButton.addEventListener("click", function AnimeAndMangaCategoryButtonClicked() {
+    console.log("Animecategory");
+});
