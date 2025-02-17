69ca856f93ea465a576961d6f5e46fa955ccb371
Sairx <ischevych@gmail.com>
2025-02-17 01:07:11 +0100

    commit

--- MainPageScript.js
+++ MainPageScript.js
@@ -1,30 +1,32 @@
-var AllCategoryButton = document.getElementById("AllCategoryButton");
-var GamingCategoryButton = document.getElementById("GamingCategoryButton");
-var SocialMediaCategoryButton = document.getElementById("SocialMediaCategoryButton");
-var ArtificialIntelligenceCategoryButton = document.getElementById("ArtificialIntelligenceCategoryButton");
-var WorldCategoryButton = document.getElementById("WorldCategoryButton");
-var AnimeAndMangaCategoryButton = document.getElementById("AnimeAndMangaCategoryButton");
 
-AllCategoryButton.addEventListener("click", function AllCategoryButtonClicked() {
-    console.log("Allcategory");
+// Get all category buttons
+var categoryButtons = document.querySelectorAll(".CategoriesButton");
+
+// Get all news containers
+var newsContainers = document.querySelectorAll(".NewsContainer");
+
+// Function to filter news based on category
+function filterNews(category) {
+    newsContainers.forEach(function (news) {
+        if (category === "All") {
+            news.style.display = "block"; // Show all news if 'All' is clicked
+        } else {
+            if (news.getAttribute("data-category") === category) {
+                news.style.display = "block"; // Show matched category
+            } else {
+                news.style.display = "none"; // Hide unmatched categories
+            }
+        }
+    });
+}
+
+// Add event listeners to each category button
+categoryButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var category = button.getAttribute("id").replace("CategoryButton", ""); // Extract category name
+        filterNews(category);
+    });
 });
 
-GamingCategoryButton.addEventListener("click", function GamingCategoryButtonClicked() {
-    console.log("Gamingcategory");
-});
-
-SocialMediaCategoryButton.addEventListener("click", function SocialMediaCategoryButtonClicked() {
-    console.log("SocialMediacategory");
-});
-
-ArtificialIntelligenceCategoryButton.addEventListener("click", function ArtificialIntelligenceCategoryButtonClicked() {
-    console.log("AIcategory");
-});
-
-WorldCategoryButton.addEventListener("click", function WorldCategoryButtonClicked() {
-    console.log("Worldcategory");
-});
-
-AnimeAndMangaCategoryButton.addEventListener("click", function AnimeAndMangaCategoryButtonClicked() {
-    console.log("Animecategory");
-});
+// Set "All" as default display
+filterNews("All");
