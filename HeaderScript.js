7b7b747834a886706c41c811808287cf6c6814fb
Sairx <ischevych@gmail.com>
2025-02-18 23:19:39 +0100

    languagechange2commit

--- HeaderScript.js
+++ HeaderScript.js
@@ -14,10 +14,22 @@ EnglishLanguageButton.addEventListener("click", function () {
     SetLanguage("English");
     EnglishLanguageButton.style.color = "rgb(220, 20, 60)";
     SpanishLanguageButton.style.color = "white";
+    LanguageChange()
 });
 
 SpanishLanguageButton.addEventListener("click", function () {
     SetLanguage("Spanish");
     EnglishLanguageButton.style.color = "white";
     SpanishLanguageButton.style.color = "rgb(220, 20, 60)";
+    LanguageChange()
 });
+
+var SignInButton = document.getElementById("SignInButton");
+function LanguageChange() {
+    let Language = localStorage.getItem("Language");
+    if (Language === "English") {
+        SignInButton.innerHTML = "Sign In";
+    } else if (Language === "Spanish") {
+        SignInButton.innerHTML = "Iniciar Sesión";
+    }
+};
