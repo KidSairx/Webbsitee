e324222eb634cd42d957a9a089ea047c9df86c28
Sairx <ischevych@gmail.com>
2025-02-22 15:59:55 +0100

    commit

--- HeaderScript.js
+++ HeaderScript.js
@@ -27,15 +27,18 @@ SpanishLanguageButton.addEventListener("click", function () {
 var SignInButton = document.getElementById("SignInButton");
 var SignInButton = document.getElementById("FooterLicense");
 var SignInButton = document.getElementById("FooterContactUs");
+var BackToMainPageButton = document.getElementById("BackToMainPageButton");
 function LanguageChange() {
     let Language = localStorage.getItem("Language");
     if (Language === "English") {
         SignInButton.innerHTML = "Sign In";
         FooterLicense.innerHTML = `This web page(everything except images) is licensed under a <br> <a href="https://creativecommons.org/licenses/by-nd/4.0/" target="_blank"> Creative Commons Attribution - NoDerivatives 4.0 International License</a>`;
         FooterContactUs.innerHTML = `Contact us at <a href="mailto:ischevych@gmail.com">ouremail@mail.com</a>`;
+        BackToMainPageButton.innerHTML = "Go Back to main page";
     } else if (Language === "Spanish") {
         SignInButton.innerHTML = "Iniciar Sesión";
         FooterLicense.innerHTML = `Esta página web(todo excepto imagenes) está licensiada bajo <br> <a href="https://creativecommons.org/licenses/by-nd/4.0/" target="_blank"> Licencia Creative Commons Atribución-SinDerivadas 4.0 Internacional</a>`;
         FooterContactUs.innerHTML = `Contacta con nosotros en <a href="mailto:ischevych@gmail.com">ouremail@mail.com</a>`;
+        BackToMainPageButton.innerHTML = "Volver a la página principal";
     }
 };
