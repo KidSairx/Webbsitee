117e85813d9614b050c5634efc428df325b7c7fd
Sairx <ischevych@gmail.com>
2025-03-08 23:58:41 +0100

    commit

--- ArticlePageScript.js
+++ ArticlePageScript.js
@@ -11,23 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 const article = data.find(article => article.CustomArticleId === articleId);
 
                 if (article) {
-                    document.querySelector("#NewsTitleText").textContent = article.headline;
                     document.querySelector("#NewsArticleMainImage").src = article.image[0];
-                    document.querySelector("#NewsDescriptionText").textContent = article.description;
-                    document.querySelector("#ArticleHeadline1").innerHTML = article.ArticleHeadline[0];
                     document.querySelector("#NewsArticleImage1").src = article.image[1];
-                    document.querySelector("#ArticleText1").innerHTML = article.ArticleBody[0];
-                    document.querySelector("#ArticleHeadline2").innerHTML = article.ArticleHeadline[1];
                     document.querySelector("#NewsArticleImage2").src = article.image[2];
-                    document.querySelector("#ArticleText2").innerHTML = article.ArticleBody[1];
-                    document.querySelector("#ArticleHeadline3").innerHTML = article.ArticleHeadline[2];
                     document.querySelector("#NewsArticleImage3").src = article.image[3];
-                    document.querySelector("#ArticleText3").innerHTML = article.ArticleBody[2];
                     document.querySelector("#ArticleAuthor").textContent = article.author.name;
                     document.querySelector("#ArticlePublishedDate").textContent = article.DatePublished;
                     document.querySelector("#ArticleLastModifiedDate").textContent = article.DateModified;
                     document.querySelector("#ArticleId").textContent = article.CustomArticleId;
-                    document.title = article.headline;
 
                     //hide images if they are not provided
                     if (article.image[1] === "") {
